2eb1f8ff1e9e84d360551b7669c16bd0b7a6b791
aca14rc <rcasabianca1@sheffield.ac.uk>
2015-12-04 15:33:43 +0000

    Finally we did something

---
+++ Address.java
@@ -0,0 +1,44 @@
+public class Address {
+	private String houseNumber;
+	private String streetName;
+	private String district;
+	private String city;
+	private String postCode;
+	public Address (String num,String n,String d,String c, String p) {
+		houseNumber = num;
+		streetName = n;
+		district = d;
+		city = c;
+		postCode = p;
+	}
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+}
--- Main.java
+++ Main.java
@@ -1,17 +1,39 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
+//import Address;
 
 public class Main{
 	public static void main (String [] args){
-		Connection con = null;
+		Connection con = null;;
 		try {
-			//Class.forName("com.mysql.jdbc.Driver");
+			//Class.forName("org.gjt.mm.mysql.Driver");
 			String dB="jdbc:mysql://stusql.dcs.shef.ac.uk/team017?user=team017&password=33b55883";
-			con = DriverManager.getConnection(dB);	
+			con = DriverManager.getConnection(dB);
+
+			String addAddress = "INSERT INTO addresses (number,street,district,city,postCode)" 
+						+ "VALUES (?,?,?,?,?)";
+
+			PreparedStatement preparedStmt = con.prepareStatement(addAddress);
+  		        preparedStmt.setString (1, "15a");
+			preparedStmt.setString (2, "Edward Street");
+			preparedStmt.setString (3, "Sheffield");
+			preparedStmt.setString (4, "Sheffield");
+			preparedStmt.setString (5, "S3 7Gb");
+		        preparedStmt.execute();
+	      
+		        con.close();
 		}
 		catch (SQLException ex) {
 			ex.printStackTrace();
 		}
+		
+		
+			
+		
+		//Address address1 = new Address("15a","Edward Street","Sheffield","Sheffield","S3 7GA");
+		
+		
 	}
 }
