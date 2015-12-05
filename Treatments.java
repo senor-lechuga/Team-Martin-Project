63212bc6072cc823b270317631565333cfe09a9b
acb14rf <rfuidge1@sheffield.ac.uk>
2015-12-05 12:35:58 +0000

    get and set for treatment

--- Treatments.java
+++ Treatments.java
@@ -6,4 +6,17 @@ public class Treatments {
 		treatmentType = type;
 		cost = c;
 	}
+	public String getTreatmentType() {
+		return treatmentType;
+	}
+	public void setTreatmentType(String treatmentType) {
+		this.treatmentType = treatmentType;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
 }
