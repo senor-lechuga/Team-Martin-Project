5f16ab3b0adf400bdd989d3eec136f6604a9cfd8
Rafael Casabianca <Rafa@dyn225116.shef.ac.uk>
2015-12-05 15:03:52 +0000

    Created Calendar object

--- Calendar.java
+++ Calendar.java
@@ -1,5 +1,46 @@
+import java.util.*;
+
 public class Calendar {
-  private String partner;
-  public Calendar ([Appointments a], String p)
-  appointment = a.
+  private ArrayList<Appointments> appointments = new ArrayList<Appointments>();
+  public Calendar (ArrayList<Appointments> apps) {
+      appointments = apps;
+  }
+
+  public ArrayList<Appointments> getAppointments(){
+      return appointments;
+  }
+
+  public void addAppointment(Appointments a){
+      int size = appointments.size();
+      for(Appointments apps : appointments){
+          if(a.getEndTime().before(apps.getStartTime())
+              || a.getStartTime().after(apps.getEndTime())
+                  && a.getPartner() == apps.getPartner()){
+                      appointments.add(a);
+                      break;
+                  }
+      }
+      if(appointments.size()>size)
+          System.out.print("Appointment has been added succesfully");
+      else
+          System.out.print("Appointment cannot be added to the calendar");
+  }
+
+  public ArrayList<Appointments> getDentistAppointments(){
+    ArrayList<Appointments> apps = new ArrayList<Appointments>();
+    for(Appointments a : apps){
+      if (a.getPartner() == "Dentist")
+          apps.add(a);
+    }
+    return apps;
+  }
+
+  public ArrayList<Appointments> getHygienistAppointments(){
+    ArrayList<Appointments> apps = new ArrayList<Appointments>();
+    for(Appointments a : apps){
+      if (a.getPartner() == "Hygienist")
+          apps.add(a);
+    }
+    return apps;
+  }
 }
