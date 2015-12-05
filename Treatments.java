aab68a2f231b08ac9d55626d665ac146dc56cb0c
Martin Cnobel <martin.cnobel@hotmail.co.uk>
2015-12-05 12:29:43 +0000

    Treatments class without get/set methods

---
+++ Treatments.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Treatments {
+	private String treatmentType;
+	private double cost;
+	public Treatments (String type,double c) {
+		treatmentType = type;
+		cost = c;
+	}
+}
