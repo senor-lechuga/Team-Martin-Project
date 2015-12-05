cbfaa34868c96e8c52168574ea3c246798ebe9dc
Martin Cnobel <martin.cnobel@hotmail.co.uk>
2015-12-05 12:18:06 +0000

    TreatmentTypes class with no get/set methods

---
+++ TreatmentTypes.java
@@ -0,0 +1,8 @@
+public class TreatmentTypes {
+	private String treatmentType;
+	private double cost;
+	public TreatmentTypes (String treatment,double c) {
+		treatmentType = treatment;
+		cost = c;
+	}
+}
