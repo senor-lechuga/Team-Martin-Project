9621543f34b8bd6c34366180c37be6db9ecb54db
acb14rf <rfuidge1@sheffield.ac.uk>
2015-12-05 12:32:11 +0000

    added get and set

--- Appointments.java
+++ Appointments.java
@@ -13,4 +13,40 @@ public class Appointments {
 		partner = partners;
 		paid = p;
 	}
+		public int getPatientID() {
+		return patientID;
+	}
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	public String getPartner() {
+		return partner;
+	}
+	public void setPartner(String partner) {
+		this.partner = partner;
+	}
+	public boolean isPaid() {
+		return paid;
+	}
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
 }
