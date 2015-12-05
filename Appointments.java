927b00b3297f2a96b6617a09ac3bf2192dc2f0ad
Martin Cnobel <martin.cnobel@hotmail.co.uk>
2015-12-05 12:14:02 +0000

    Errors fixed

--- Appointments.java
+++ Appointments.java
@@ -1,14 +1,16 @@
+import java.util.*;
 public class Appointments {
 	private int patientID;
-	private Date date;
-	private Time startTime, endTime;
+	public Date date;
+	private Date startTime, endTime;
 	private String partner;
 	private boolean paid;
-	public Appointments (int id,Date dates,Time sTime,Time eTime, String partners, boolean p) {
+	public Appointments (int id,Date dates,Date sTime,Date eTime, String partners, boolean p) {
 		patientID = id;
 		date = dates;
 		startTime = sTime;
-		endTime = eTime
+		endTime = eTime;
 		partner = partners;
 		paid = p;
 	}
+}
