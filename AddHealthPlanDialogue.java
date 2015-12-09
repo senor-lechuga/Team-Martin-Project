5e7227775a9a087066076435eca0315e4552198d
Rafael Casabianca <Rafa@dyn227189.shef.ac.uk>
2015-12-09 17:53:28 +0000

    Added feature so that the GUI adds health plan to database

--- AddHealthPlanDialogue.java
+++ AddHealthPlanDialogue.java
@@ -1,31 +1,33 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class AddHealthPlanDialogue extends JFrame {
-	
-	private JButton createBtn, cancelBtn;
-	private JTextField planNameField;
-	private JSpinner checkupCount, hygienesCount, repairsCount;
-	private SqlHandler handler;
 
-	public AddHealthPlanDialogue (SqlHandler h)
+	private JButton createBtn, cancelBtn;
+	private JTextField planNameField,costCount;
+	private JSpinner checkupCount, hygienesCount, repairsCount;
+	private String name;
+	private int checkup,hygiene,repairs;
+	private double cost;
+	private HealthcarePlan plan=null;
+
+	public AddHealthPlanDialogue ()
 	{
-		//Set up sql handler
-		handler = h;
 		//Set up the panel
-		super("Add new Health Plan");
+		//super("Add new Health Plan");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//TODO: Get rid of this, just make it close the window.
-		
+
 		Container pane = this.getContentPane();
-		pane.setLayout(new GridLayout(5,1));
+		pane.setLayout(new GridLayout(6,1));
 
 		JPanel planName = new JPanel();
 		planName.setLayout(new FlowLayout()/* new GridLayout(2,1)*/);
 		planName.add(new JLabel("Plan Name:"));
 		planNameField = new JTextField(30);
 		planName.add(planNameField);
-		
+
 		SpinnerModel sm = new SpinnerNumberModel(0, 0, 365, 1);
 		JPanel checkUps = new JPanel();
 		checkUps.setLayout(new FlowLayout());
@@ -45,18 +47,27 @@ public class AddHealthPlanDialogue extends JFrame {
 		repairsCount = new JSpinner(new SpinnerNumberModel(0, 0, 365, 1));
 		repairs.add(repairsCount);
 
+		JPanel monthlyCost = new JPanel();
+		monthlyCost.setLayout(new FlowLayout());
+		monthlyCost.add(new JLabel("Monthly cost of plan:"));
+		costCount = new JTextField(5);
+		monthlyCost.add(costCount);
+
 		JPanel bottomButtons = new JPanel();
 		bottomButtons.setLayout(new FlowLayout()/* new GridLayout(1,2)*/);
 		createBtn = new JButton("Create");
-		cancelBtn = new JButton("Cancel");	
+		cancelBtn = new JButton("Cancel");
 		bottomButtons.add(createBtn);
 		bottomButtons.add(cancelBtn);
+
+
 
 		// Add all elements to the panel
 		pane.add(planName);
 		pane.add(checkUps);
 		pane.add(hygienes);
 		pane.add(repairs);
+		pane.add(monthlyCost);
 		pane.add(bottomButtons);
 		this.pack();
 		setLocationRelativeTo(null);// Display in the centre of the screen
@@ -66,6 +77,11 @@ public class AddHealthPlanDialogue extends JFrame {
 		createBtn.addActionListener(createListener);
 		this.setVisible( true );
 	}
+
+	public HealthcarePlan getPlan(){
+		return plan;
+	}
+
 	private ActionListener cancelListener = new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
@@ -87,7 +103,14 @@ public class AddHealthPlanDialogue extends JFrame {
 			{
 				//Perform SQL stuff here to create the new entry
 				System.out.println("Add a new healthcare plan with these details:");
-				System.out.println("|Name: " + planNameField.getText() + "\n|# of Checkups: " + checkupCount.getValue() + "\n|# of Hygiene checks: " + hygienesCount.getValue() + "\n|# of repairs: " + repairsCount.getValue());
+				System.out.println("|Name: " + planNameField.getText() + "\n|# of Checkups: " + checkupCount.getValue() + "\n|# of Hygiene checks: " + hygienesCount.getValue() + "\n|# of repairs: " + repairsCount.getValue() + "\n|Monthly cost: " + costCount.getText());
+				//Creates Healthplan object
+				name = planNameField.getText();
+				checkup = Integer.valueOf((Integer)checkupCount.getValue());
+				hygiene = Integer.valueOf((Integer)hygienesCount.getValue());
+				repairs = Integer.valueOf((Integer)repairsCount.getValue());
+				cost = Double.parseDouble(costCount.getText());
+				plan = new HealthcarePlan(name,checkup,hygiene,repairs,cost);
 				//Finally, close the window:
 				closeWindow();
 			}
@@ -100,8 +123,10 @@ public class AddHealthPlanDialogue extends JFrame {
 		this.dispose();
 	}
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws SQLException
 	{
 		AddHealthPlanDialogue d = new AddHealthPlanDialogue();
+		SqlHandler h = new SqlHandler();
+		h.addHealcarePlan(d.getPlan());
 	}
 }
