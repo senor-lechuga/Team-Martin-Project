819adc5630d3410a1b04d2125680dd2544408daf
unknown <aca14mc@DCS04119.windows.dcs.shef.ac.uk>
2015-12-14 15:20:41 +0000

    Combo box fixed on monthly choice

--- MonthChoice.java
+++ MonthChoice.java
@@ -297,6 +297,7 @@ public class MonthChoice extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(MonthChoice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
