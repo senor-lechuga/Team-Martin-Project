ccfa8ba11ca89379d8a1a749d796cf3da1eda057
Blayze Millward <bfmillward1@sheffield.ac.uk>
2015-12-16 13:05:33 +0000

    Added comments to AddPatientDialogue

--- AddPatientDialogue.java
+++ AddPatientDialogue.java
@@ -6,21 +6,27 @@ import java.sql.*;
 import java.util.Date;
 import java.text.*;
 
+/**
+ * AddPatientDialogue
+ * This dialogue allows the secretary to register patients into the system
+ */
+
 public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanEditor {
 	
+	//Create variable for the GUI elements
 	private JButton createBtn, cancelBtn, addAddressBtn, addPlanBtn;
 	private JTextField firstName, lastName, phoneNumber;
 	private JSpinner birthDay, birthMonth, birthYear;
 	private JComboBox address, healthcarePlan, title;
-	private SqlHandler handler;
+	private SqlHandler handler;// Store a reference to the SQL database handler
 
 	public AddPatientDialogue (SqlHandler h)
 	{
-		super("Register a new patient");
-		//Set up sql handler
+		super("Register a new patient");// Setup the window's title
+		//Set up sql handler by referencing the main handler
 		handler = h;
 
-		//Set up the panel	
+		//Set up the panel contents
 		Container pane = this.getContentPane();
 		pane.setLayout(new GridLayout(8,1));
 
@@ -108,6 +114,8 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 		setResizable(false);
 	}
 
+	// The two functions below are used to properly populate the address and healthplan comboboxes
+	// This means that they can be updated from other classes
 	public void setupAddressBox()
 	{
 		Address[] addresses;
@@ -131,6 +139,7 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 		healthcarePlan.setModel(model);
 	}
 
+	// Action listeners to spawn add dialogues
 	private ActionListener addressPopup = new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
@@ -147,6 +156,7 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 			d.addPlanEditorReference(AddPatientDialogue.this);
 		}
 	};
+	// Action listener to close the window when cancel is clicked
 	private ActionListener cancelListener = new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
@@ -155,22 +165,26 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 			closeWindow();
 		}
 	};
+	// Action listener to create the user when cliced
 	private ActionListener createListener = new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
 		{
+			// Create a string composed of a list of error messages and populate it as each error occurs:
 			String errorMessage = "";
 			String sFirstName = firstName.getText().replaceAll("\\s+", "");
 			String sLastName = lastName.getText().replaceAll("\\s+", "");
 			String sPhoneNumber = phoneNumber.getText().replaceAll("\\s+", "");
-			if(sFirstName.equals("") || sLastName.equals(""))
+			if(sFirstName.equals("") || sLastName.equals(""))// Make sure there's a first name
 				errorMessage += "\nPlease enter both a first and last name.";
+			// Make sure there's a properly formatted phone number:
 			if(sPhoneNumber.length() > 3 && sPhoneNumber.substring(0,3).equals("+44"))
 				sPhoneNumber = "0"+sPhoneNumber.substring(3);
 			if(sPhoneNumber.length() != 11)
 				errorMessage += "\nThe phone number is not the correct length.";
 			else if(!sPhoneNumber.matches("[0-9]+"))
 				errorMessage +="\nThe phone number contains non-numbers.";
+			// Make sure the date of birth is reasonable:
 			String dateString = String.valueOf(birthDay.getValue()) + "-" + String.valueOf(birthMonth.getValue()) + "-" + String.valueOf(birthYear.getValue());
 			try{
 				DateFormat date = new SimpleDateFormat("dd-MM-yyyy");
@@ -185,10 +199,11 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 			}catch(ParseException ex) {
 				errorMessage += "\nPlease enter a valid birth day.";
 			}
-			if(errorMessage != "")// There is a formatting error
+			// If there was an error, display it as an error message, if not, then add the data to the database:
+			if(errorMessage != "")//error
 				JOptionPane.showMessageDialog(null, errorMessage.substring(1), "Incorrect Data", JOptionPane.WARNING_MESSAGE);
-			else{
-				// Make a new patient here
+			else{// Push to database
+				// Make a new patient object
 				int intBirthYear = Integer.valueOf(String.valueOf(birthYear.getValue()));
 				int intBirthMonth = Integer.valueOf(String.valueOf(birthMonth.getValue()));
 				int intBirthDay = Integer.valueOf(String.valueOf(birthDay.getValue()));
@@ -196,24 +211,27 @@ public class AddPatientDialogue extends JFrame implements IAddressEditor, IPlanE
 				HealthcarePlan hp = (HealthcarePlan)healthcarePlan.getSelectedItem();
 				Address add = (Address)address.getSelectedItem();
 				Patient p = new Patient(String.valueOf(title.getSelectedItem()), sFirstName, sLastName, sqlBirthDate, sPhoneNumber, hp, add);
+				// Attempt to push it to the database:
 				try {
 					handler.addPatient(p);
 				}catch(SQLException ex){
 					JOptionPane.showMessageDialog(null, "Error registering patient.", "ERROR", JOptionPane.ERROR_MESSAGE);
 				}
-				closeWindow();
+				closeWindow();//Finally, close the window
 			}
 
 			
 		}
 	};
 
+	//Close window function, only closes the GUI elements
 	private void closeWindow()
 	{
 		this.setVisible(false);
 		this.dispose();
 	}
 
+	// Test harness
 	public static void main(String[] args)
 	{
 		AddPatientDialogue d = new AddPatientDialogue(null);
