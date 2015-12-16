3133a022b478a8f417ae12b7f57086bff6abb269
Blayze Millward <bfmillward1@sheffield.ac.uk>
2015-12-16 13:12:47 +0000

    Added comments to AddAddressDialogue, testing next.

--- AddAddressDialogue.java
+++ AddAddressDialogue.java
@@ -3,20 +3,26 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.sql.SQLException;
 
+/**
+ * AddAddressDialogue
+ * Allows for the creation of a new address, which can then be associated with patients
+ */
+
 public class AddAddressDialogue extends JFrame {
 	
+	//Create the GUI elements
 	private JButton createBtn, cancelBtn;
 	private JTextField number, street, district, city, postCode;
-	private SqlHandler handler;
-	private IAddressEditor patientCreator;
+	private SqlHandler handler;//Reference to the SQL database
+	private IAddressEditor patientCreator;// reference to the editor that created this, if set
 
 	public AddAddressDialogue (SqlHandler h)
 	{
-		super("Add new address");
+		super("Add new address");// Title the window
 		//Set up the SQL stuff
-		handler = h;
+		handler = h;//Store reference to the database connection
 
-		//Set up the panel
+		//Set up the panel components
 		Container pane = this.getContentPane();
 		pane.setLayout(new GridLayout(6,1));
 
@@ -50,7 +56,6 @@ public class AddAddressDialogue extends JFrame {
 		postCode = new JTextField(30);
 		postCodePanel.add(postCode);
 
-
 		JPanel bottomButtons = new JPanel();
 		bottomButtons.setLayout(new FlowLayout());
 		createBtn = new JButton("Create");
@@ -75,11 +80,14 @@ public class AddAddressDialogue extends JFrame {
 		setResizable(false);
 	}
 
+	// This function allows the linking of an IAddressEditor object, an object that contains a combobox that
+	// will change after this GUI has been worked through.
 	public void addAddressEditorReference(IAddressEditor d)
 	{
 		patientCreator = d;
 	}
 
+	// Action Listeners:
 	private ActionListener cancelListener = new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
@@ -88,19 +96,17 @@ public class AddAddressDialogue extends JFrame {
 			closeWindow();
 		}
 	};
-	private ActionListener createListener = new ActionListener()
+	private ActionListener createListener = new ActionListener()// Action listener for creation of a new address
 	{
 		public void actionPerformed(ActionEvent e)
 		{
 			//Perform pre-evaluation here to make sure entered fields are correct
-//			String escapedPlanName = planName.getText();  Apparently escaping is not needed when using prepared statements, as I think we are?
 			String sPostCode = postCode.getText().replaceAll("\\s+","");// Strip spaces out of the postCode
 			if(number.getText().equals("") || postCode.getText().equals(""))
 				JOptionPane.showMessageDialog(null, "Please enter at least a house number and post code.", "Data missing", JOptionPane.WARNING_MESSAGE);
 			else
 			{
-				//Perform SQL stuff here to create the new entry
-				
+				//Perform SQL handling to create the new entry, by pushing the new Address object
 				Address a = new Address(number.getText(), street.getText(), district.getText(), city.getText(), sPostCode);
 				try{
 					handler.setAddress(a);
@@ -116,12 +122,14 @@ public class AddAddressDialogue extends JFrame {
 		}
 	};
 
+	// A simple close function that removes all GUI components
 	private void closeWindow()
 	{
 		this.setVisible(false);
 		this.dispose();
 	}
 
+	// Test harness
 	public static void main(String[] args)
 	{
 		AddAddressDialogue d = new AddAddressDialogue(null);
